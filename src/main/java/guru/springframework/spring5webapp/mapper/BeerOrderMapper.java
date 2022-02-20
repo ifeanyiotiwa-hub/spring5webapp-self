e725e9fafe7cec861ca6c3413c57f9578cf76b6c
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-20 22:22:26 +0100

    modifies BeerOrderMapper.java

--- src/main/java/guru/springframework/spring5webapp/mapper/BeerOrderMapper.java
+++ src/main/java/guru/springframework/spring5webapp/mapper/BeerOrderMapper.java
@@ -3,7 +3,9 @@ package guru.springframework.spring5webapp.mapper;
 import guru.springframework.spring5webapp.dto.BeerOrderDTO;
 import guru.springframework.spring5webapp.model.BeerOrder;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
 
