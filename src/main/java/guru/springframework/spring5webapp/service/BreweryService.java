65b13a65250ee949b342566cfe66240fc9776e95
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-20 17:56:33 +0100

    adds service class

---
+++ src/main/java/guru/springframework/spring5webapp/service/BreweryService.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5webapp.service;
+
+import guru.springframework.spring5webapp.model.Brewery;
+
+import java.util.List;
+
+public interface BreweryService {
+    List<Brewery> getAllBreweries();
+}
